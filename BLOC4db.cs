ce6ebe01a50abfd88fea6eea0bf5e47d41b23c32
DoubleRiichi <DoubleRiichi>
2024-12-22 11:17:10 +0100

    Ajout de la classe DBContext permettant la connexion à la base de données.
    Changement dans appsettings, ajout d'une connexion string pour la connexion mysql

---
+++ BLOC4db.cs
@@ -0,0 +1,15 @@
+﻿namespace BLOC4_API
+{
+    using Microsoft.EntityFrameworkCore;
+    using BLOC4_API.Models;
+
+    public class BLOC4db : DbContext
+    {
+        public BLOC4db(DbContextOptions<BLOC4db> options) : base(options) { }
+
+        public DbSet<Connexion> Connexion { get; set; }
+        public DbSet<Salaries> Salaries { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<Sites> Sites { get; set; }
+    }
+}
